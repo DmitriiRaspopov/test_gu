b32a59a73c6c10ecf6b09b2e07e9a0df63d38c92
Dmitrii <44145387+dimas71bit@users.noreply.github.com>
2021-12-03 11:50:22 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+from PIL import Image 
+import PIL 
+
+uploaded_files = st.file_uploader("Загрузите изображения с других камер для того, чтобы узнать где есть мусор", 
+       accept_multiple_files=True)
+for uploaded_file in uploaded_files:
+    im = Image.open(uploaded_file)
+    st.image(im)
+    im1 = im.save("img/1.jpg")
